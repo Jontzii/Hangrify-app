e36fe75cec3e30cc1dbccfaa15b75121073c1e94
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2022-06-13 09:42:23 +0300

    Initial Commit

---
+++ Hangrify/HangrifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangrifyApp.swift
+//  Hangrify
+//
+//  Created by Joonas Hiltunen on 13.6.2022.
+//
+
+import SwiftUI
+
+@main
+struct HangrifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
